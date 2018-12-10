e3cf11ca93b6e68f5fcce32ac7341802b8d11c74
Shilpa Patwary <shipatwa@publicisgroupe.net>
2018-12-10 22:29:24 +0530

    Adding unit test for API's

---
+++ test/workspaces.test.js
@@ -0,0 +1,212 @@
+const request = require('supertest');
+const chai = require('chai');
+const mocha = require('mocha');
+const app = require('../appServer');
+const Workspaces = require('../model/workspaceModel');
+const Channels = require('../model/channelModel');
+
+const should = chai.should();
+
+function loginUser() {
+  return request(app)
+    .post('/auth/login')
+    .type('form')
+    .send({
+      username: 'shilpap',
+      password: '12345',
+    });
+}
+
+mocha.describe('Slack Application', () => {
+  mocha.before((done) => {
+    loginUser();
+    done();
+  });
+  mocha.it('should retrieve all workspaces', () => {
+    request(app)
+      .get('/api/workspaces/')
+      .expect('content-type', /json/)
+      .expect(200)
+      .end((error, res) => {
+        res.body.should.be.a('array');
+        should.not.exist(error);
+        should.exist(res);
+      });
+  });
+  mocha.it('should retrieve a single workspace', () => {
+    request(app)
+      .get('/api/workspaces/23140')
+      .expect('content-type', /json/)
+      .expect(200)
+      .end((error, res) => {
+        res.body.should.be.a('object');
+        should.not.exist(error);
+        should.exist(res);
+      });
+  });
+  mocha.it('should update a workspace', () => {
+    const updatedWorkspace = {
+      id: `${Math.floor(Math.random() * 100000)}`,
+      name: 'workspace_old',
+    };
+    const workspace = new Workspaces(updatedWorkspace);
+    workspace.save((err, ws) => {
+      request(app)
+        .put(`/api/workspaces/${ws.id}`)
+        .send({
+          id: '12345',
+          name: 'workspace_new',
+        })
+        .expect('content-type', /json/)
+        .expect(200)
+        .end((error, res) => {
+          should.not.exist(error);
+          res.body.should.be.a('object');
+          res.body.should.have.property('name').eql('workspace_new');
+          should.exist(res);
+        });
+    });
+  });
+  mocha.it('should delete a board', () => {
+    const newWorkspace = {
+      id: `${Math.floor(Math.random() * 100000)}`,
+      name: 'workspace_new',
+    };
+    const workspace = new Workspaces(newWorkspace);
+    workspace.save((err, ws) => {
+      request(app)
+        .delete(`/api/workspaces/${ws.id}`)
+        .expect('content-type', /json/)
+        .expect(200)
+        .end((error, res) => {
+          should.not.exist(error);
+          res.body.should.be.a('object');
+          should.exist(res);
+        });
+    });
+  });
+  mocha.it('should create a workspace', () => {
+    request(app)
+      .post('/api/workspaces/')
+      .send({
+        id: `${Math.floor(Math.random() * 100000)}`,
+        name: 'corporate_social',
+        users: [{
+          id: 'user1',
+          name: 'Shilpa',
+        }],
+        channels: [
+          {
+            id: 'ioferfj',
+            name: 'random',
+            users: [],
+          },
+        ],
+      })
+      .expect('content-type', /json/)
+      .expect(201)
+      .end((error, res) => {
+        should.not.exist(error);
+        res.body.should.have.property('name');
+        res.body.should.have.property('users');
+        res.body.should.have.property('channels');
+        should.exist(res);
+      });
+  });
+  mocha.it('should add users in a workspace', () => {
+    const newWorkspace = {
+      id: `${Math.floor(Math.random() * 100000)}`,
+      name: 'workspace_old',
+      users: [],
+      channels: [],
+    };
+    const workspace = new Workspaces(newWorkspace);
+    workspace.save((err, ws) => {
+      request(app)
+        .put(`/api/workspaces/${ws.id}/users`)
+        .send({
+          id: '12345',
+          name: 'Shilpa',
+        })
+        .expect('content-type', /json/)
+        .expect(200)
+        .end((error, res) => {
+          should.not.exist(error);
+          res.body.users.length.should.not.be.eql(0);
+          should.exist(res);
+        });
+    });
+  });
+  mocha.it('should add channles in workspace', () => {
+    const newWorkspace = {
+      id: `${Math.floor(Math.random() * 100000)}`,
+      name: 'workspace_old',
+      users: [],
+      channels: [],
+    };
+    const workspace = new Workspaces(newWorkspace);
+    workspace.save((err, ws) => {
+      request(app)
+        .put(`/api/workspaces/${ws.id}/channels`)
+        .send({
+          id: '12345',
+          name: 'channel1',
+        })
+        .expect('content-type', /json/)
+        .expect(200)
+        .end((error, res) => {
+          should.not.exist(error);
+          res.body.channels.length.should.not.be.eql(0);
+          should.exist(res);
+        });
+    });
+  });
+  mocha.it('should add users in channel', () => {
+    const newChannel = {
+      id: `${Math.floor(Math.random() * 100000)}`,
+      name: 'channel1',
+      users: [],
+      messages: [],
+    };
+    const channel = new Channels(newChannel);
+    channel.save((err, ch) => {
+      request(app)
+        .put(`/api/channels/${ch.id}/users`)
+        .send({
+          id: '98888',
+          name: 'user1',
+        })
+        .expect('content-type', /json/)
+        .expect(200)
+        .end((error, res) => {
+          should.not.exist(error);
+          res.body.users.length.should.not.be.eql(0);
+          should.exist(res);
+        });
+    });
+  });
+  mocha.it('should add messages in channel', () => {
+    const newChannel = {
+      id: `${Math.floor(Math.random() * 100000)}`,
+      name: 'channel1',
+      users: [],
+      messages: [],
+    };
+    const channel = new Channels(newChannel);
+    channel.save((err, ch) => {
+      request(app)
+        .put(`/api/channels/${ch.id}/messages`)
+        .send({
+          id: '12345',
+          message: 'hi',
+        })
+        .expect('content-type', /json/)
+        .expect(200)
+        .end((error, res) => {
+          should.not.exist(error);
+          res.body.messages.length.should.not.be.eql(0);
+          should.exist(res);
+        });
+    });
+  });
+});
