433698e44426f34edbc579b192c5bf2b365ae033
Shilpa Patwary <shipatwa@publicisgroupe.net>
2018-12-17 16:37:32 +0530

    Initial commit to dockerize the application

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const devDbUrl = 'mongodb://localhost:27017/myappdb';
+const devDbUrl = 'mongodb://mongo:27017/myappdb';
 const mongoURI = process.env.MONGOLAB_URI || devDbUrl;
 
-mongoose.connect(mongoURI, (err) => {
+mongoose.connect(devDbUrl, (err) => {
   if (err) {
     throw (err);
   } else {
